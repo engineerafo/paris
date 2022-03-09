d5361dbe6fe7859e7c1f5c178ac30c3c8b336a2e
engineerafo <34166365+engineerafo@users.noreply.github.com>
2022-03-09 18:22:39 +0200

    Update Function.java

--- src/main/java/javafunction/Function.java
+++ src/main/java/javafunction/Function.java
@@ -20,7 +20,7 @@ public class Function {
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("Http1")
+    @FunctionName("CapFunction")
     public HttpResponseMessage run(
             @HttpTrigger(
                 name = "req",
